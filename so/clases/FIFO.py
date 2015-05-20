8d12f0993b9a7508bac8907963bc2cc706ea539b
Federico <federico.lochbaum@live.com>
2015-05-19 21:04:44 -0300

    agregue el Timer en el roundRobin.. falta agregar el recorrido a ready de ese proceso

--- so/clases/FIFO.py
+++ so/clases/FIFO.py
@@ -1,14 +1,14 @@
 from clases import SchedullingPolitic
 
 class FIFO(SchedullingPolitic):
-    def __init__(self):
-        pass
+    def __init__(self,queuesManager):
+        self.queuesManager = queuesManager
         
 
 
-    def next(self,colaReady):
-        first = colaReady.first()
-        colaReady.deque()
+    def next(self):
+        first = self.queuesManager.getReadyQueue().first()
+        self.queuesManager.getReadyQueue().deque()
         return first
 
         
--- so/clases/RoundRobin.py
+++ so/clases/RoundRobin.py
@@ -1,15 +1,19 @@
-from clases import SchedullingPolitic
+from clases import SchedullingPolitic, Timer
 
 
 class RoundRobin(SchedullingPolitic):
-    def __init__(self,cantDeTiempoProc):
-        
-        self.cantDeTiempoProc=cantDeTiempoProc
+    def __init__(self,quantum,queuesManager):
+        self.quantum = quantum
+        self.queuesManager = queuesManager
+        self.temp = Timer(self.quantum)
         
 
         
     def next(self):
-        '''ya veremos'''
+        first = self.queuesManager.getReadyQueue().first()
+        self.queuesManager.getReadyQueue().deque()
+        self.temp.restart()
+        return first
 
 
         
