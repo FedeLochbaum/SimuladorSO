6c680d72ea96de3c6802c179e635df3296b1aceb
Federico <federico.lochbaum@live.com>
2015-05-24 14:56:05 -0300

    no se porque no anda aun el envejecer.. deberia

--- so/clases/ReadyQueuePriority.py
+++ so/clases/ReadyQueuePriority.py
@@ -9,7 +9,7 @@ class ReadyQueuePriority:
         self.ready = []
 
     def put(self,elemet):
-        self.ready.append((elemet.getPriority(), elemet))
+        self.ready.append([elemet.getPriority(), elemet])
         heapify (self.ready)
 
    
@@ -21,18 +21,20 @@ class ReadyQueuePriority:
     
     def envejecerHeap(self):
         for  i,(priority,item)  in  enumerate(self.ready):
-            self.ready[i]=(priority-1,item)
+            self.ready[i]=[priority-1,item]
+
     
 i = ReadyQueuePriority()
 i.put(Pcb('proceso',1,0,1,0,22))
 i.put(Pcb('proceso',1,0,1,0,22))
-#i.put(Pcb('proceso',1,0,1,0,1))
-#i.put(Pcb('proceso',1,0,1,0,3))
-#i.put(Pcb('proceso',1,0,1,0,4))
-#i.put(Pcb('proceso',1,0,1,0,2))
-#i.put(Pcb('proceso',1,0,1,0,5))
-#print (i.next().getPriority())
-#print (i.next().getPriority())
-#print (i.next().getPriority())
-#print (i.next().getPriority())
-#print (i.next().getPriority())          
+i.put(Pcb('proceso',1,0,1,0,1))
+i.put(Pcb('proceso',1,0,1,0,3))
+i.put(Pcb('proceso',1,0,1,0,4))
+i.put(Pcb('proceso',1,0,1,0,2))
+i.put(Pcb('proceso',1,0,1,0,5))
+
+print (i.next().getPriority())
+print (i.next().getPriority())
+print (i.next().getPriority())
+print (i.next().getPriority())
+print (i.next().getPriority())          
