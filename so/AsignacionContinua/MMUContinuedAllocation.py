377dbf4a39d6ea1f5277b4e660b89c382f0642b3
Federico <federico.lochbaum@live.com>
2015-06-21 15:14:14 -0300

    agregados en MMUContinuedAllocation

--- so/AsignacionContinua/MMUContinuedAllocation.py
+++ so/AsignacionContinua/MMUContinuedAllocation.py
@@ -4,16 +4,29 @@ class MMUContinuedAllocation(MemoryManager):
     def __init__(self,memory,routineBlock):
         self.super.__init__(memory)
         self.bloquesDisponibles = [Block(1,memory.space)]
+        self.bloquesNoDisponibles = []
         self.routine = routineBlock
         
+    def loadProgram(self,pcb):
+        if(self.memoryFree()>pcb.getFinalPc):
+            if(self.hayBloqueDisponible()):
+                bloque = self.traerBloquePara(pcb.getFinalPc())
+                self.agregarAmemoria(pcb,bloque)
+            else:
+                self.compactarMemory()
+                self.loadProgram(pcb)
         
     def agregarAmemoria(self,pcb):
         
+            
     
     def hayBloqueDisponible(self,pcb):
-        
+        ***iterar self.bloquesDisponibles hay que ver que ed usamos para los bloques :D 
+            result = result or (each.size > pcb.getFinalPc())
+        return result
     
-    def traerBloqueSegunRutina(self,pcb):
-        return self.routine.dameBloquePara(pcb.getFinalPc(),sef.bloquesDisponibles)
-                
+    def traerBloqueSegunRutina(self,cantidad):
+        bloque =  self.routine.dameBloquePara(cantidad,sef.bloquesDisponibles)
+        self.reordenarBloque(bloque)
+        return bloque        
         
